6af5649f1b78129bb7f46f0d01656e3741ae9cd9
Ryo Ota <nwtgck@gmail.com>
2019-01-18 21:48:51 +0900

    [add] Add --insecure, -k options

--- cmd/post.go
+++ cmd/post.go
@@ -9,33 +9,44 @@ import (
 	"os"
 )
 
+var postInsecure bool
+
 func init() {
 	RootCmd.AddCommand(postCmd)
+
+	// Flags
+	// NOTE: --insecure, -k is inspired by curl
+	postCmd.Flags().BoolVarP(&postInsecure, "insecure", "k", false, "Allow insecure server connections when using SS")
+}
+
+// Generate HTTP client
+func getHttpClient() *http.Client {
+	// Set insecure or not
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{ InsecureSkipVerify: postInsecure },
+	}
+	return &http.Client{Transport: tr}
+
 }
 
 var postCmd = &cobra.Command{
 	Use:   "post",
 	Short: "HTTP POST method",
 	RunE: func(cmd *cobra.Command, args []string) error {
-
 		if len(args) != 1 {
 			return fmt.Errorf("URL should be specified")
 		}
-
 		// Get server URL
 		// TODO: improve usability to fill "https://" automatically
 		serverUrlStr := args[0]
-
 		// Read from stdin
 		// NOTE: reader can be change in the future by options
 		reader := os.Stdin
 		// TODO: Hard code
 		contentType := "application/octet-stream"
-		// TODO: Hard code
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{ InsecureSkipVerify: true },
-		}
-		httpClient := http.Client{Transport: tr}
+		// Get HTTP client
+		httpClient := getHttpClient()
+		// Post
 		resp, err := httpClient.Post(serverUrlStr, contentType, reader)
 		if err != nil {
 			return err
